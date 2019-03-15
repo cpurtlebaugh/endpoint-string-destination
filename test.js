a1edc715e4edb20f10853bc9ac1d9b0ab57582ed
Casper Purtlebaugh <casperpurtlebaugh@gmail.com>
2019-03-15 13:02:01 -0700

    initial commit

---
+++ test.js
@@ -0,0 +1,104 @@
+'use strict';
+const input = `
+CREATE fruits
+CREATE vegetables
+CREATE grains
+CREATE fruits/apples
+CREATE fruits/apples/fuji
+LIST
+CREATE grains/squash
+MOVE grains/squash vegetables
+CREATE foods
+MOVE grains foods
+MOVE fruits foods
+MOVE vegetables foods
+LIST
+DELETE fruits/apples
+DELETE foods/fruits/apples
+LIST`
+
+const exOutput = `
+CREATE fruits
+CREATE vegetables
+CREATE grains
+CREATE fruits/apples
+CREATE fruits/apples/fuji
+LIST
+fruits
+  apples
+    fuji
+grains
+vegetables
+CREATE grains/squash
+MOVE grains/squash vegetables
+CREATE foods
+MOVE grains foods
+MOVE fruits foods
+MOVE vegetables foods
+LIST
+foods
+  fruits
+    apples
+      fuji
+  grains
+  vegetables
+    squash
+DELETE fruits/apples
+Cannot delete fruits/apples - fruits does not exist
+DELETE foods/fruits/apples
+LIST
+foods
+  fruits
+  grains
+  vegetables
+    squash
+`
+
+function createOutput(input){
+	const arr = input.split(" ").join("").split("");
+	let action = "";
+	let destination = "";
+	let final = {};
+
+	arr.forEach((el, i) => {
+		// IF UPPERCASE - CREATES ACTION
+		if(el.match(/[A-Z]/)){
+			action += el;
+		
+		// IF LOWERCASE OR SLASH - CREATE DESTINATION
+		} else if(el.match(/[a-z]|[/]/)) {
+			destination += el
+		}else {
+		// IF NEITHER - EXECUTES ACTION OF DESTINATION
+			switch(true) {
+				case action === "CREATE":
+				console.log("destination: ", destination)
+					let tempDest = destination.split("/");
+					// need to loop through and check previous value in tempdest arr
+					// to see if it's a valid object key name in final or
+					final[tempDest[0]] = {}
+					break;
+				case action === "DELETE":
+					// check if that object key exists, if so re-write final object
+					// if not skip
+					break;
+				case action === "MOVE":
+					// console.log("MOVE DESTINATION", destination)
+					break;
+				case action === "LIST":
+					// print that portion of the final object and it's children
+			  }
+			// clear temporarily stored values
+			action = "";
+			destination = "";
+		}
+
+		return final;
+	});
+}
+
+// RUN SCRIPT
+(async function testOutput(){
+	return createOutput(input)
+})();
+
